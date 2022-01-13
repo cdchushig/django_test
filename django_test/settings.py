3cc4fbd8fbc6dd2813e35e129cbabcee08566597
cdchushig <cdavid.chushig@gmail.com>
2022-01-14 00:07:10 +0100

    Added .env file

--- django_test/settings.py
+++ django_test/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-c-m^gq@$9vi)to3heh=&+prma2ddz=u%ccqpk5qq&*fq=rr^!5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('DIAGRAM_GURU_ALLOWED_HOSTS').split(",")
 
 
 # Application definition
