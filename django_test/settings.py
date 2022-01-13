c3665e016040c01da0e5978526eee1e7fcd57157
cdchushig <cdavid.chushig@gmail.com>
2022-01-14 00:11:00 +0100

    Updated project

--- django_test/settings.py
+++ django_test/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-c-m^gq@$9vi)to3heh=&+prma2ddz=u%ccqpk5qq&*fq=rr^!5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [localhost, 127.0.0.1, 0.0.0.0]
+ALLOWED_HOSTS = [localhost, '127.0.0.1', '0.0.0.0']
 
 # Application definition
 
